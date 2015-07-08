71c031f022f4698db322a52e642db33076ce3378
Luciano Fiore <l.fiore@live.com>
2015-07-08 01:06:28 +0100

    fixed friendly URLs
    friendly URLs weren't working correctly with a script path specified

--- public/common.php
+++ public/common.php
@@ -1,10 +1,12 @@
 <?php
 
-$main_site_url = trim($site_url, '/') . '/' . ($script_path ? trim($script_path, '/') . '/' : '');
+$main_site_url = trim($site_url, '/') . '/';
 
-$view_url = 'http://' . $main_site_url . ($friendly_urls ? '' : 'view.php?id=');
+$main_script_path = ($script_path ? trim($script_path, '/') . '/' : '');
 
-$images_url = 'http://' . ($friendly_urls ? 'i.' : '') . $main_site_url . ($friendly_urls ? '' : 'images/');
+$view_url = 'http://' . $main_site_url . $main_script_path . ($friendly_urls ? '' : 'view.php?id=');
+
+$images_url = 'http://' . ($friendly_urls ? 'i.' : '') . $main_site_url . ($friendly_urls ? '' : $main_script_path . 'images/');
 
 $in_script = true;
 
