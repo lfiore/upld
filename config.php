ec8034be8fed17d70312a86b68ad8b89cd14d5ac
Luciano Fiore <lfiore@users.noreply.github.com>
2017-07-26 20:10:55 +0100

    added anon uploads and url whitelisting features

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+
+define('SITE_NAME', 'yoursite.com'); // site name displayed in the header and FAQ/ToS
+
+define('SITE_URL', 'admin@email.com'); // the domain ONLY for your site (i.e. without the "upld" folder)
+define('SCRIPT_PATH', ''); // folder where the script is located (leave blank if it's in the root directory)
+
+// email for contact page
+define('CONTACT_EMAIL', 'contact@yoursite.com'); // the email on the contact page
+// email to get reports
+define('REPORT_EMAIL', 'reports@yoursite.com'); // where emails should be sent if someone reports an image
+
+// use domain.com/imageID and i.domain.com/imageID.ext instead of domain.com/view.php?id=imageID and domain.com/images/imageID.ext for shorter URLs?
+// WARNING: ONLY enable this if you have configured your webserver to rewrite URLs - see INSTALL.txt
+define('FRIENDLY_URLS', false);
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'username');
+define('DB_PASS', 'password');
+define('DB_NAME', 'upld');
+define('DB_PORT', 3306); // usually 3306 by default. If you don't know what this is, leave it alone and ask your hosting company if it doesn't work
+
+// allow remote downloads? GD (php5-gd) MUST be enabled for this to work
+define('ALLOW_REMOTE', true);
+
+// max size in bytes - remember that you might need to change this in your PHP config file too
+define('ALLOWED_SIZE', 2000000); // 1000 = 1 kilobyte, 1000000 = 1 megabyte
+
+// allow anonymous uploads? set to 0 false to only allow logged in members to upload
+define('ANON_UPLOADS', true);
+
+// set to true to only allow image downloads from whitelisted URLs (below)
+define('URL_WHITELIST', false);
+
+$allowed_urls = [
+	'i.imgur.com',
+	'i.upld.eu'
+];
+
---
+++ main.php
@@ -0,0 +1,131 @@
+<?php
+
+if (!defined('IN_SCRIPT'))
+{
+	header('location: ../index.php');
+	exit;
+}
+
+$i = 0;
+$allowed_size = ALLOWED_SIZE;
+
+while ($allowed_size >= 1000)
+{
+	$allowed_size = ($allowed_size / 1000);
+	++$i;
+}
+
+$units = array('', 'K', 'M');
+
+$size = round($allowed_size, 1) . $units[$i];
+
+?>
+
+		<div class="box">
+
+			<p class="title">Welcome to <?php echo SITE_NAME; ?></p>
+
+			<ul>
+				<li><span class="black"><?php echo SITE_NAME; ?></span> is a free, online image host. Simply click the button below to start uploading!</li>
+				<li>Before uploading, you can register an account (or log in if you already have one) and manager your uploads later</li>
+			</ul>
+
+		</div>
+
+		<div class="box">
+
+			<p class="title">Why use <?php echo SITE_NAME; ?>?</p>
+
+			<ul>
+				<li>It's completely <span class="black">free</span>!</li>
+				<li>You can create an account and <span class="black">manage all of your uploads</span></li>
+<?php
+
+if (ALLOW_REMOTE === true)
+{
+
+?>
+
+				<li><span class="black">Download files remotely!</span></li>
+
+<?php
+
+}
+
+?>
+
+				<li>The following image types are allowed: <span class="black">PNG, JPG, GIF</span></li>
+				<li>The files may be up to <span class="black"><?php echo $size ?>B</span> in size</li>
+<?php
+
+if (FRIENDLY_URLS === true)
+{
+
+?>
+
+				<li><span class="black">Short, easy to remember</span> URLs!</li>
+
+<?php
+
+}
+
+?>
+
+				<li></li>
+			</ul>
+		</div>
+
+<?php
+
+if ((ANON_UPLOADS === true) || ((ANON_UPLOADS === false) && (isset($_SESSION['user']))))
+{
+
+?>
+
+		<div id="select-image" class="box">
+			click here to select an image
+		</div>
+
+		<form id="upload-form" class="hidden" name="upload" method="POST" action="upload.php" enctype="multipart/form-data">
+			<input id="image-input" name="image" type="file" />
+		</form>
+
+		<div id="cancel-image" class="hidden">
+			<span>wait, I want to upload something else!</span>
+		</div>
+
+<?php
+
+	if (ALLOW_REMOTE === true)
+	{
+
+?>
+
+		<form id="url-form" name="remote-url" method="POST" action="upload.php">
+
+			<div id="download-url" class="box">
+				<input id="image-url-submit" type="submit" value="download remote image" />
+			</div>
+
+			<div id="select-url" class="box">
+				<input id="select-url-input" name="url" type="text" placeholder="Want to download your image remotely? Paste the link here (http://)" />
+			</div>
+		</form>
+
+<?php
+
+	}
+
+}
+else
+{
+
+?>
+
+		<div class="box">Anonymous uploads have been disabled, please <a href="register.php">create an account</a> or <a href="login.php">log in</a> to upload</div>
+
+<?php
+
+}
+
+?>
